d120a7a145c650bda18f1dc806e466f49228b713
Lyn Headley <laheadle@gmail.com>
2014-09-06 08:56:18 -0700

    move the list of entries and outcomes into the toplevel file for each tournament. Don't keep a big fat file of all events

--- play_tennis.ml
+++ play_tennis.ml
@@ -1,8 +1,7 @@
 
-let play event outcomes =
-  let db = Tennis_player.make_db () in
+let play entries outcomes =
 
-  let tourney = Tourney.init (event db) in
+  let tourney = Tourney.init entries in
 
   let current_state = List.fold_left (fun tourney win -> Tennis_tourney.won_str tourney win)
 	tourney outcomes
